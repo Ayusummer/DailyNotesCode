cd9a549947ec32e8aedfed864a6a7df725e666f9
咸鱼型233 <ayusummer233@qq.com>
2022-11-22 23:56:20 +0800

    ✨ Go语言圣经-命令行参数

--- Go/main.go
+++ Go/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	hello "GoLearning/pkg/hello"
+	"GoLearning/pkg/cmd_param"
+	"fmt"
 )
 
 func main() {
-
-	// hello.PrintHelloWorld()
-	// hello.PrintHelloVSCode()
-	hello.SemiColon()
+	fmt.Println("echo 基本写法:")
+	cmd_param.Print_cmd_args()
+	fmt.Println("echo 切片写法:")
+	cmd_param.Echo_Slice()
+	fmt.Println("echo strings.Join() 写法:")
+	cmd_param.Echo_Join()
+	fmt.Println("echo 直接打印切片:")
+	cmd_param.Echo_direct_print_slice()
 }
---
+++ Go/pkg/cmd_param/cmd_test.go
@@ -0,0 +1,8 @@
+package cmd_param
+
+import "testing"
+
+func TestCmd(t *testing.T) {
+	t.Log("切片测试:")
+	Slice_test()
+}
