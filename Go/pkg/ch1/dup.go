358d307feb6713dd0e15c02c07ed7c6d7d4a24d1
咸鱼型233 <ayusummer233@qq.com>
2023-01-10 23:06:23 +0800

    🐛 1.3-计算重复行-捉个虫-替换掉弃用包

--- Go/pkg/ch1/dup.go
+++ Go/pkg/ch1/dup.go
@@ -3,7 +3,6 @@ package ch1
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -72,7 +71,8 @@ func Dup2() {
 func Dup3() {
 	counts := make(map[string]int)
 	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+		// data, err := ioutil.ReadFile(filename)
+		data, err := os.ReadFile(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
