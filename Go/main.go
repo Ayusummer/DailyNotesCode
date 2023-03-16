c6d01aba262548aa2935f1aa4dd2ae9451f445bb
233PC <ayusummer233@qq.com>
2023-03-17 01:09:54 +0800

    Go 语言圣经 -  练习 1.9 输出状态码

--- Go/main.go
+++ Go/main.go
@@ -7,5 +7,6 @@ import (
 func main() {
 	// ch1.PrintResponseBody()
 	// ch1.PrintResponseBody_Copy()
-	ch1.PrintResponseBody_Copy_Prefix()
+	// ch1.PrintResponseBody_Copy_Prefix()
+	ch1.PrintResponseBody_Copy_Prefix_Status()
 }
---
+++ Go/pkg/ch1/ex1-9.go
@@ -0,0 +1,40 @@
+/*
+练习 1.9
+修改 fetch 打印出HTTP协议的状态码，可以从 resp.Status 变量得到该状态码。
+*/
+package ch1
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func PrintResponseBody_Copy_Prefix_Status() {
+	for _, url := range os.Args[1:] { // 遍历命令行参数中的每个URL
+		// 如果输入的url参数没有 http:// 前缀的话，为这个url加上该前缀
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+			fmt.Printf("输入的url参数没有 http:// 前缀,已为该url加上该前缀\n当前url为: %s\n", url)
+		}
+		resp, err := http.Get(url) // 发送HTTP GET请求并获取响应
+		// 如果有错误发生，打印错误信息并退出程序并返回错误码1
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close() // 关闭响应体
+
+		// 打印HTTP协议的状态码
+		fmt.Printf("HTTP协议的状态码: %s", resp.Status)
+
+		n, err := io.Copy(os.Stdout, resp.Body) // 读取响应体
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Copied %d bytes \n", n)
+	}
+}
