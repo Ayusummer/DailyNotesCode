811916b1f7b6c3d9aa2469538aa9448fa0756611
233Official <ayusummer233@gmail.com>
2023-11-03 15:19:08 +0800

    python - dns server - 补充个参考材料

--- Python/use_case/dns_server/query_dns.ps1
+++ Python/use_case/dns_server/query_dns.ps1
@@ -5,3 +5,5 @@ Resolve-DnsName -Name $domain -Server $dns
 # 
 Resolve-DnsName -Name example.com -Server 8.8.8.8
 Resolve-DnsName -Name yahoo.com -Server 127.0.0.1
+
+# 参考材料记录: https://github.com/rthalley/dnspython/tree/master
