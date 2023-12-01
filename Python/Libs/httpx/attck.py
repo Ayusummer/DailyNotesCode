4b51eb968033f4a1c734a50a9a9abfff292dda6a
233Official <ayusummer233@gmail.com>
2023-12-01 16:06:00 +0800

    httpx - proxy

--- Python/Libs/httpx/attck.py
+++ Python/Libs/httpx/attck.py
@@ -5,6 +5,9 @@ url = "https://attack.mitre.org/matrices/enterprise/"
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
+# 使用本地 7890 端口作为代理
+proxies = {"http": "http://127.0.0.1:7890"}
+
 # 发送请求(忽略证书验证)
-r = httpx.get(url, headers=headers, verify=False)
+r = httpx.get(url, headers=headers, verify=False, proxies="http://127.0.0.1:7890")
 print(r.text)
