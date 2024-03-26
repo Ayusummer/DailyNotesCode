e2e01b6622c4be26525eba01c6f34b2cc744f8a9
233Dev <ayusummer233@gmail.com>
2024-03-26 18:50:46 +0800

    sstash

---
+++ Go/Character/CH1/CH1-7-Server/main.go
@@ -0,0 +1,19 @@
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func main() {
+    http.HandleFunc("/", handler) // each request calls handler
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
